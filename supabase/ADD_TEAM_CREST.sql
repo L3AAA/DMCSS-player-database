91edf2ad9b77e1d96a52e5276fa9a924e75ab087
M3Ad <aandrewstt@gmail.com>
2025-11-24 15:26:53 -0400

    Fix team crest alignment and remove white border

--- supabase/ADD_TEAM_CREST.sql
+++ supabase/ADD_TEAM_CREST.sql
@@ -2,4 +2,4 @@
 ALTER TABLE player_card ADD COLUMN IF NOT EXISTS team_crest text;
 
 -- Set default team crest for all existing cards
-UPDATE player_card SET team_crest = '/DMCSS-crest.png' WHERE team_crest IS NULL;
+UPDATE player_card SET team_crest = '/DMCSS crest.png' WHERE team_crest IS NULL;
